4ee514af47ea6cabe28fd55914329964e2f0037f
diana <dianadibah11@gmail.com>
2023-05-27 22:22:10 +0300

    secound version

--- auth/login.php
+++ auth/login.php
@@ -6,11 +6,12 @@ $email = filterRequest("email");
 $password = sha1($_POST["password"]);
 
 
-$stmt = $con->prepare("SELECT * FROM users WHERE users_email=?  AND users_password= ? And users_approve=1 ");
-$stmt->execute(array($email, $password));
-$count = $stmt->rowCount();
+//$stmt = $con->prepare("SELECT * FROM users WHERE users_email=?  AND users_password= ? And users_approve=1 ");
+//$stmt->execute(array($email, $password));
+//$count = $stmt->rowCount();
 
-result($count);
+//result($count);
+getdate("users","users_email=?  AND users_password= ? And users_approve=1",array($email, $password));
 
 
 
