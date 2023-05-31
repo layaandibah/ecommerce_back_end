90ae700b9fcc850a6e569335cabf0d34ac7b4b55
diana <dianadibah11@gmail.com>
2023-05-31 23:36:42 +0300

    final homeview  backend

--- home.php
+++ home.php
@@ -6,10 +6,14 @@ $alldata=array();
 $alldata["status"]="success";
 
 $allcategories=getAllData("categories",null,null,false);
-$allitemsdiscount=getAllData("itemsview","items_discount!=0",null,false);
+$allitemsdiscount=getAllData("itemsview","items_discount!=0 AND items_active=1",null,false);
+$allitemssoldout=getAllData("itemsview","items_active=0 AND items_discount=0",null,false);
+$allitemsdiscountandsoldout=getAllData("itemsview","items_active=0 AND items_discount!=0",null,false);
 
 $alldata["categories"]=$allcategories;
 $alldata["itemsdiscount"]=$allitemsdiscount;
+$alldata["itemssoldout"]=$allitemssoldout;
+$alldata["itemsdiscountsoldout"]=$allitemsdiscountandsoldout;
 
 echo json_encode($alldata);
 ?>
