a4c8bb4064a4e6e7d030adc166b8ad5d7d55657c
diana <dianadibah11@gmail.com>
2023-06-06 23:40:24 +0300

    update home

--- home.php
+++ home.php
@@ -6,30 +6,18 @@ $alldata=array();
 $alldata["status"]="success";
 
 $allcategories=getAllData("categories",null,null,false);
-$allitemsdiscount=getAllData("allitems","items_discount!=0 AND items_active=1",null,false);
-$allitemssoldout=getAllData("allitems","items_active=0 AND items_discount=0",null,false);
-$allitemsdiscountandsoldout=getAllData("allitems","items_active=0 AND items_discount!=0",null,false);
-if($allcategories==null && $allitemsdiscount ==null && $allitemssoldout==null && $allitemsdiscountandsoldout==null){
+$allitems=getAllData("allitems",null,null,false);
+if($allcategories==null && $allitems==null){
     $alldata["status"]="failure";
     }else{
     $alldata["status"]="success";
     if($allcategories!=null){
         $alldata["categories"]=$allcategories;
     }
-    if($allitemsdiscount!=null){
-        $alldata["itemsdiscount"]=$allitemsdiscount;
+    if($allitems!=null){
+        $alldata["items"]=$allitems;
     }
-    if($allitemsdiscountandsoldout!=null){
-        $alldata["itemsdiscountsoldout"]=$allitemsdiscountandsoldout;
-    }
-    if($allitemssoldout!=null){
-        $alldata["itemssoldout"]=$allitemssoldout;
-    }
-    }
-//$alldata["categories"]=$allcategories;
-//$alldata["itemsdiscount"]=$allitemsdiscount;
-//$alldata["itemssoldout"]=$allitemssoldout;
-//$alldata["itemsdiscountsoldout"]=$allitemsdiscountandsoldout;
 
+    }
 echo json_encode($alldata);
 ?>
